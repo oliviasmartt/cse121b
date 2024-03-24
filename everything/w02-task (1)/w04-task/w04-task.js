6ae1b6458084bc64795c8161382ce5b297d9f66e
oliviasmartt <sma21005@byui.edu>
2024-03-23 22:35:35 -0600

    week 1 and 2 fixed

---
+++ everything/w02-task (1)/w04-task/w04-task.js
@@ -0,0 +1,45 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+    name: "Olivia Smart",
+    photo: "",
+    favoriteFoods: [
+        'Pizza',
+        'Hamburgers',
+        'Steak',
+        'French Fries'
+    ],
+};
+
+myProfile.placesLived.push(
+    {
+        place: 'Rancho Cucmonga, CA',
+        length: ' 18 years',
+        place: 'Rexburg, ID',
+        length: '3 years'
+
+    }
+)
+
+/* Populate Profile Object with placesLive objects */
+
+
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+
+/* Photo with attributes */
+
+
+/* Favorite Foods List*/
+
+
+/* Hobbies List */
+
+
+/* Places Lived DataList */
+
+
